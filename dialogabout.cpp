e5531e661fd0956a90b4f9c4bda054fbc47ef958
jerome05230 <jerome.baroncampos@gmail.com>
2017-04-07 09:23:31 +0200

    Update dialogabout.cpp

--- dialogabout.cpp
+++ dialogabout.cpp
@@ -1,13 +1,16 @@
 #include "dialogabout.h"
 #include "ui_dialogabout.h"
+#include <QDebug>
 
 DialogAbout::DialogAbout(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogAbout)
 {
+    qDebug() << "DialogAbout::DialogAbout(QWidget *parent)";
     ui->setupUi(this);
 }
 
+qDebug() << "DialogAbout::~DialogAbout()";
 DialogAbout::~DialogAbout()
 {
     delete ui;
@@ -15,5 +18,6 @@ DialogAbout::~DialogAbout()
 
 void DialogAbout::on_buttonBox_accepted()
 {
+   qDebug() << "void DialogAbout::on_buttonBox_accepted()";
    close();
 }

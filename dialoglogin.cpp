e546144bb454e953cca697eed498569358588ecd
jerome05230 <jerome.baroncampos@gmail.com>
2017-04-07 09:22:03 +0200

    Update dialoglogin.cpp

--- dialoglogin.cpp
+++ dialoglogin.cpp
@@ -8,28 +8,35 @@ DialogLogin::DialogLogin(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogLogin)
 {
+    qDebug() << "DialogLogin::DialogLogin(QWidget *parent)";
     ui->setupUi(this);
 }
 
 DialogLogin::~DialogLogin()
 {
+    qDebug() << "~DialogLogin()";
     delete ui;
 }
 
-void DialogLogin::activatedConnexionButton(){
+void DialogLogin::activatedConnexionButton()
+{
+    qDebug() << "void DialogLogin::activatedConnexionButton()";
     ui->pushButtonConnexion->setEnabled(ui->lineEditLogin->text()!=""&& ui->lineEditPassword->text()!="");
 }
 void DialogLogin::on_lineEditLogin_textChanged()
 {
+    qDebug() << "void DialogLogin::on_lineEditLogin_textChanged()";
     activatedConnexionButton();
 }
 
 void DialogLogin::on_lineEditPassword_textChanged()
 {
+    qDebug() << "void DialogLogin::on_lineEditPassword_textChanged()";
     activatedConnexionButton();
 }
 void DialogLogin::on_pushButtonConnexion_clicked()
 {
+    qDebug() << "void DialogLogin::on_pushButtonConnexion_clicked()";
     QString login=ui->lineEditLogin->text();
     QString password=ui->lineEditPassword->text();
     QString serveur=ui->lineEditServeur->text();
@@ -50,6 +57,7 @@ void DialogLogin::on_pushButtonConnexion_clicked()
 
 void DialogLogin::on_pushButtonCancel_clicked()
 {
+    qDebug() << "void DialogLogin::on_pushButtonCancel_clicked()";
     qDebug() << "au revoir" << endl;
     reject();
 }
